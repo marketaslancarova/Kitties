08e27ecdeed6c15492270380c034a18de79bd824
marketaslancarova <marketaslancarova@gmail.com>
2024-12-03 10:32:09 +0100

    Fetch cat images and handle loading and error states; update rendering logic

--- kitties/src/App.js
+++ kitties/src/App.js
@@ -4,35 +4,53 @@ import "./App.css";
 import { cats } from "./utils/cats";
 
 function App() {
-  const [catsImageUrl, setCatsImageUrl] = useState([]);
+  const [catsWithImage, setCatsWithImageUrl] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchCatsWithImage = async () => {
+    try {
+      const response = await fetch(
+        "https://api.thecatapi.com/v1/images/search?limit=10"
+      );
+      const catsImage = await response.json();
+
+      return cats.map((cat, index) => ({
+        ...cat,
+        imageUrl: catsImage[index]?.url || "/images/default.jpg",
+      }));
+    } catch (error) {
+      setError(error);
+    }
+  };
 
   useEffect(() => {
-    const fetchImages = async () => {
-      try {
-        const response = await fetch(
-          "https://api.thecatapi.com/v1/images/search?limit=10"
-        );
-        const data = await response.json();
-        setCatsImageUrl(data);
-      } catch (error) {
-        console.log(error);
-      }
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      const catsWithImage = await fetchCatsWithImage();
+      setCatsWithImageUrl(catsWithImage);
+      setLoading(false);
     };
 
-    fetchImages();
+    fetchData();
   }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
 
   return (
     <div className="App">
-      {console.log(catsImageUrl)}
       <h1>Cats</h1>
+
       <ul>
-        {cats.map((cat, index) => (
+        {catsWithImage.map((cat, index) => (
           <li key={index}>
             <h2>{cat.name}</h2>
             <p>Age: {cat.age}</p>
             <p>Years: {cat.years}</p>
             <p>Trait: {cat.trait}</p>
+            <img src={cat.imageUrl} alt={cat.name} />
           </li>
         ))}
       </ul>
